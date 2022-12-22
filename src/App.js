b5497de5c34fe929d351954290832d98fb28c70c
jesuszvl <jesus.zavala@gametime.co>
2022-12-21 21:53:48 -0800

    reactify site

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="navbar-main">
+          <img className="navbar-logo" src="makahco.jpg" alt="makahco" />
+          <span className="navbar-title">makahco</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
