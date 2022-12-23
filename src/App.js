71b2eb577790a5042345084de79d770500958380
jesuszvl <jesus.zavala@gametime.co>
2022-12-22 20:19:46 -0800

    add hero and description

--- src/App.js
+++ src/App.js
@@ -6,10 +6,34 @@ function App() {
       <div className="navbar">
         <div className="navbar-main">
           <img className="navbar-logo" src="makahco.png" alt="makahco" />
-          <span className="navbar-title">makahco</span>
         </div>
       </div>
-      <div className="hero"></div>
+      <div className="hero">
+        <h1 className="hero-title">Makahco Studio</h1>
+      </div>
+      <div className="about">
+        <div className="about-content">
+          <p>
+            Welcome to <b>Makahco Studio!</b> We are a team of talented
+            professionals who are passionate about helping our clients bring
+            their creative visions to life. Our services include software
+            engineering, music recording, and video production
+          </p>
+          <p>
+            Whether you need a{" "}
+            <b>
+              custom software solution, high-quality audio recording, or
+              professional video production,
+            </b>{" "}
+            we have the skills and experience to help you go bananas over your
+            project.
+          </p>
+          <p>
+            So don't monkey around, get in touch with us today and let's
+            ape-peal to your creative side!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
