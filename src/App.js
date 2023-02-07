44083afaf2b4116d49c81bd9018ab39335fe1d3f
jesuszvl <jesus.zavala@gametime.co>
2023-02-06 23:09:21 -0800

    add GA tracking

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import ReactGA from "react-ga";
+
 import "./App.css";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
 import Gallery from "./components/Gallery/Gallery";
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
+
+ReactGA.initialize("G-4KN7RH6T6R");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
