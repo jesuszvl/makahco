c8efc9902188c631c0defdcb6dd485caf3e805e6
jesuszvl <jesus.zavala@gametime.co>
2023-06-15 03:59:55 -0700

    add buttons to cards

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
       <h1>Contact Us</h1>
       <div className={styles["about-content"]}>
         <p>
-          Ready to start? Send an email to <b>info@makah.co</b>
+          Ready to start? Send an email to <b>info@makahco.com</b>
         </p>
       </div>
     </div>
