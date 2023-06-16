b07b7ede0728495739d495cf33f44e2c04552e54
jesuszvl <jesus.zavala@gametime.co>
2023-06-16 02:13:36 -0700

    add spotify player

--- pages/beats.js
+++ pages/beats.js
@@ -16,7 +16,19 @@ export default function Beats() {
         <meta name="description" content="Creative Studio"></meta>
       </Head>
       <Navbar />
-      <div className="content">🎧</div>
+      <div className="content">
+        <h3>Not ready yet... Listen to my fav jams in the meantime!</h3>
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/playlist/2NzyWV9sT9rnKMncFHCkJG?utm_source=generator&theme=0"
+          width="100%"
+          height="500"
+          frameBorder="0"
+          allowFullScreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        />
+      </div>
       <Footer />
     </div>
   );
