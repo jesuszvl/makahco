c65ba7e87ce133ea55e258d96317f3a26ffb0073
jesuszvl <jesus.zavala@gametime.co>
2023-07-09 05:40:31 -0700

    add button icons

--- pages/free.js
+++ pages/free.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
-import useSound from "use-sound";
-import PageContainer from "../src/components/PageContainer/PageContainer";
-import { trackPageView } from "../src/utils/analytics";
+import Image from "next/image";
 
-import styles from "../src/styles/Free.module.scss";
+import PageContainer from "../src/components/PageContainer/PageContainer";
 import FreeModeSelector from "../src/components/FreeModeSelector/FreeModeSelector";
 import { MODES } from "../src/utils/constants";
 import { getRandomSpanishWord } from "./api/randomWords";
+import { trackPageView } from "../src/utils/analytics";
+
+import { Howl } from "howler";
+
+import styles from "../src/styles/Free.module.scss";
+import playIcon from "../src/icons/PlayIcon.svg";
+import stopIcon from "../src/icons/StopIcon.svg";
+import classNames from "classnames";
 
 trackPageView("/free");
 
@@ -18,35 +24,49 @@ const songs = [
   "https://kigaieivrduiqliboora.supabase.co/storage/v1/object/public/beats/base_5.mp3",
 ];
 
-const getRandomSongUrl = () => {
-  const randomIndex = Math.floor(Math.random() * songs.length);
-  return songs[randomIndex];
-};
-
 export default function Free() {
+  const getRandomSongUrl = () => {
+    const randomIndex = Math.floor(Math.random() * songs.length);
+    return songs[randomIndex];
+  };
+
   const [currentMode, setCurrentMode] = useState(MODES[0]);
   const [currentWord, setCurrentWord] = useState("-");
   const [intervalId, setIntervalId] = useState(null);
-  const [play, { stop }] = useSound(getRandomSongUrl());
+  const [soundId, setSoundId] = useState("");
+  const [soundObj, setSoundObj] = useState(null);
+
+  const wordInterval = currentMode.id === "FIVE" ? 5000 : 10000;
 
   useEffect(() => {
     if (intervalId) {
       const interval = setInterval(async () => {
         const randomWord = await getRandomSpanishWord();
         setCurrentWord(randomWord);
-      }, 10000); // Generate a new word every 10 seconds
+      }, wordInterval); // Generate a new word every 10 seconds
 
       return () => clearInterval(interval);
     }
-  }, [intervalId]);
+  }, [intervalId, wordInterval]);
 
   const handlePlay = () => {
-    play();
+    const sound = new Howl({
+      src: [getRandomSongUrl()],
+      html5: true,
+      volume: 0.7,
+    });
+
+    const id = sound.play();
+    setSoundId(id);
+    setSoundObj(sound);
+
     setIntervalId(1);
   };
 
   const handleStop = () => {
-    stop();
+    if (soundId) {
+      soundObj.stop(soundId);
+    }
     setCurrentWord("-");
     setIntervalId(null);
   };
@@ -55,21 +75,29 @@ export default function Free() {
     setCurrentMode(mode);
   };
 
+  const isBigWord = currentWord.length > 10;
+
   return (
     <PageContainer title="Makahco | Free">
       <div className="content">
         <FreeModeSelector currentMode={currentMode} onModeClick={onModeClick} />
         <div className={styles["mode-content"]}>
-          <h2 className={styles["mode-word"]}>{currentWord.toUpperCase()}</h2>
+          <p
+            className={classNames(styles["mode-word"], {
+              [styles["mode-big-word"]]: isBigWord,
+            })}
+          >
+            {currentWord.toUpperCase()}
+          </p>
         </div>
         <div className={styles["mode-controls"]}>
           {intervalId ? (
             <button className={styles["mode-button"]} onClick={handleStop}>
-              Stop
+              <Image src={stopIcon} alt="Stop" width={32} height={32} />
             </button>
           ) : (
             <button className={styles["mode-button"]} onClick={handlePlay}>
-              Play
+              <Image src={playIcon} alt="Play" width={32} height={32} />
             </button>
           )}
         </div>
