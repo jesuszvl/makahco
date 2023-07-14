509e4c98658a3ff0cad480044d75126b337ad349
jesuszvl <jesus.zavala@gametime.co>
2023-07-14 15:27:10 -0700

    remove contact

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,27 +5,29 @@ import styles from "./Hero.module.scss";
 export default function Hero() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className={styles["hero"]}>
-      <h1 className={styles["hero-title"]}>
-        <span>Ape-pealing</span>
-        <span>To The</span>
-        <span>Creative You</span>
-      </h1>
-      <button
-        className={styles["hero-cta"]}
-        onClick={() => {
-          setIsOpen(true);
-        }}
-      >
-        Join Our Troop!
-      </button>
+    <div className={styles["hero-container"]}>
+      <div className={styles["hero"]}>
+        <h1 className={styles["hero-title"]}>
+          <span>Ape-pealing</span>
+          <span>To The</span>
+          <span>Creative You</span>
+        </h1>
+        <button
+          className={styles["hero-cta"]}
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          Join Our Troop!
+        </button>
 
-      <LoginModal
-        isOpen={isOpen}
-        onHide={() => {
-          setIsOpen(false);
-        }}
-      />
+        <LoginModal
+          isOpen={isOpen}
+          onHide={() => {
+            setIsOpen(false);
+          }}
+        />
+      </div>
     </div>
   );
 }
