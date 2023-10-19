fbaf47f48806f51a708c72dcd9decf724c38685d
jesuszvl <jesus.zavala@gametime.co>
2023-10-19 02:49:48 -0700

    update song urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,33 +4,36 @@ export const MODES = [
   { id: "IMG", label: "Imágenes" },
 ];
 
+const BASE_URL =
+  "https://fsivvquhttqtxpvvfyla.supabase.co/storage/v1/object/public/beats/";
+
 export const beats = [
   {
-    src: "https://kigaieivrduiqliboora.supabase.co/storage/v1/object/public/beats/base_1.mp3",
+    src: BASE_URL + "base_1.mp3",
     beat_drop: 20,
     bpm: 94,
     spb: 10,
   },
   {
-    src: "https://kigaieivrduiqliboora.supabase.co/storage/v1/object/public/beats/base_2.mp3",
+    src: BASE_URL + "base_2.mp3",
     beat_drop: 24,
     bpm: 79,
     spb: 12,
   },
   {
-    src: "https://kigaieivrduiqliboora.supabase.co/storage/v1/object/public/beats/base_3.mp3",
+    src: BASE_URL + "base_3.mp3",
     beat_drop: 24,
     bpm: 81,
     spb: 11,
   },
   {
-    src: "https://kigaieivrduiqliboora.supabase.co/storage/v1/object/public/beats/base_4.mp3",
+    src: BASE_URL + "base_4.mp3",
     beat_drop: 21,
     bpm: 90,
     spb: 11,
   },
   {
-    src: "https://kigaieivrduiqliboora.supabase.co/storage/v1/object/public/beats/base_5.mp3",
+    src: BASE_URL + "base_5.mp3",
     beat_drop: 19,
     bpm: 100,
     spb: 10,
