005eb1028835c0e0c7087c60a9646a518bca057a
jesuszvl <jesus.zavala@gametime.co>
2023-11-15 23:23:13 -0800

    add functionality to setting selector

--- src/components/SettingSelector/SettingSelector.test.tsx
+++ src/components/SettingSelector/SettingSelector.test.tsx
@@ -2,31 +2,9 @@ import SettingSelector from "./SettingSelector";
 import { render, screen } from "@testing-library/react";
 
 describe("SettingSelector", () => {
-  test("should render setting selector for Mode correctly", () => {
-    render(
-      <SettingSelector
-        type="modo"
-        setting="clasico"
-        onSettingClick={() => {}}
-        onBack={() => {}}
-        onNext={() => {}}
-      />
-    );
+  test("should render setting selector correctly", () => {
+    render(<SettingSelector />);
 
-    expect(screen.getByText("clasico")).toBeDefined();
-  });
-
-  test("should render setting selector for Beat correctly", () => {
-    render(
-      <SettingSelector
-        type="beat"
-        setting="reggae"
-        onSettingClick={() => {}}
-        onBack={() => {}}
-        onNext={() => {}}
-      />
-    );
-
-    expect(screen.getByText("reggae")).toBeDefined();
+    expect(screen.getByText("CLÁSICO")).toBeDefined();
   });
 });
--- src/components/SettingSelector/SettingSelector.tsx
+++ src/components/SettingSelector/SettingSelector.tsx
@@ -1,30 +1,46 @@
+import { useBeatStore } from "../../store/beatStore";
+import { Setting } from "../../types/types";
 import styles from "./SettingSelector.module.scss";
 
-type SettingSelectorProps = {
-  setting: string;
-  type: string;
-  onBack: () => void;
-  onNext: () => void;
-  onSettingClick: () => void;
-};
+const SettingSelector = () => {
+  const { setting, updateSetting, getCurrentSettingValue, openModal } =
+    useBeatStore();
+  const settings: Setting[] = Object.values(Setting);
 
-const SettingSelector = ({
-  type,
-  setting,
-  onBack,
-  onNext,
-  onSettingClick,
-}: SettingSelectorProps) => {
+  const nextSetting = () => {
+    const index = settings.indexOf(setting);
+    const nextIndex = (index + 1) % settings.length;
+    return settings[nextIndex];
+  };
+
+  const previousSetting = () => {
+    const index = settings.indexOf(setting);
+    const previousIndex = (index - 1 + settings.length) % settings.length;
+    return settings[previousIndex];
+  };
+
   return (
     <div className={styles["setting-selector-container"]}>
-      <button className={styles["selector-button"]} onClick={onBack}>
+      <button
+        className={styles["selector-button"]}
+        onClick={() => {
+          updateSetting(previousSetting());
+        }}
+      >
         {"<<"}
       </button>
-      <button className={styles["selector-button"]} onClick={onSettingClick}>
-        <span className={styles["selector-button-setting"]}>{setting}</span>
-        <span className={styles["selector-button-type"]}>{type}</span>
+      <button className={styles["selector-button"]} onClick={openModal}>
+        <span className={styles["selector-button-type"]}>{setting}</span>
+        <span className={styles["selector-button-setting"]}>
+          {getCurrentSettingValue()}
+        </span>
       </button>
-      <button className={styles["selector-button"]} onClick={onNext}>
+      <button
+        className={styles["selector-button"]}
+        onClick={() => {
+          updateSetting(nextSetting());
+        }}
+      >
         {">>"}
       </button>
     </div>
